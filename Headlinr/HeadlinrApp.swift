8dcc1b1fae4772f73eaa35070fb389aefbe26217
Maisie Ng <maisieng@Mac.home>
2025-03-01 14:45:38 -0800

    Initial commit

---
+++ Headlinr/HeadlinrApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlinrApp.swift
+//  Headlinr
+//
+//  Created by Maisie Ng on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HeadlinrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeadlinrTests/HeadlinrTests.swift
@@ -0,0 +1,16 @@
+//
+//  HeadlinrTests.swift
+//  HeadlinrTests
+//
+//  Created by Maisie Ng on 3/1/25.
+//
+
+import Testing
+
+struct HeadlinrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
