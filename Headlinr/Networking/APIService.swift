ff350e2e8200e2ee7af62f90d6a3cb959ef28970
Maisie Ng <maisieng@Mac.home>
2025-03-03 16:32:21 -0800

    Add error handling for API requests

--- Headlinr/Networking/APIService.swift
+++ Headlinr/Networking/APIService.swift
@@ -9,5 +9,35 @@
 import Foundation
 
 
+enum Result {
+    case success
+    case failure(error: NetworkError)
+}
+
+enum NetworkError: Error {
+    case invalidRequest
+    case invalidResponse
+    case decodeError
+    case serverError
+    case unknown
+    
+    
+    // MARK: - Error Description
+    var localizedDescription: String {
+        switch self {
+        case .invalidRequest:
+            return "The request is invalid. Please check the URL and parameters."
+        case .invalidResponse:
+            return "The response is invalid."
+        case .decodeError:
+            return "Failed to decode the response. Please check the data format."
+        case .serverError:
+            return "The server encountered an error. Please try again later."
+        case .unknown:
+            return "An unknown error occurred."
+        }
+    }
+}
+
 
 
