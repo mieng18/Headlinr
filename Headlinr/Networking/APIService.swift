5a65fc3d429a9542cc7870f7612ecf654af24a01
Maisie Ng <maisieng@Mac.home>
2025-03-03 16:39:03 -0800

    Implement NetworkService

--- Headlinr/Networking/APIService.swift
+++ Headlinr/Networking/APIService.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-enum Result {
+enum NetworkResult {
     case success
     case failure(error: NetworkError)
 }
@@ -41,3 +41,54 @@ enum NetworkError: Error {
 
 
 
+class NetworkService {
+    static let shared = NetworkService()
+    
+    private init() {}
+    
+    func fetchRequest(client: EndpointProtocol) async throws -> Data {
+        guard let request = client.request else {
+            throw NetworkError.invalidRequest
+        }
+        
+        let config = URLSessionConfiguration.default
+        let (data, response) = try await URLSession(configuration: config).data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        // Check Response Status
+        switch checkResponse(httpResponse) {
+        case .success:
+            return data
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    /// Checks HTTP status codes and returns appropriate error
+    private func checkResponse(_ response: HTTPURLResponse) -> NetworkResult {
+        switch response.statusCode {
+        case 200...299:
+            return .success
+        case 401...500:
+            return .failure(error: .serverError)
+        default:
+            return .failure(error: .unknown)
+        }
+    }
+    
+    /// Fetch and decode JSON data
+    func fetchData<T: Decodable>(client: EndpointProtocol, type: T.Type) async throws -> T {
+        let data = try await fetchRequest(client: client)
+        let jsonDecoder = JSONDecoder()
+        
+        do {
+            let objects = try jsonDecoder.decode(T.self, from: data)
+            return objects
+        } catch {
+            throw NetworkError.decodeError
+        }
+    }
+}
