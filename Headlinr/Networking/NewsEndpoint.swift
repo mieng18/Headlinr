dd6c7d8db67349c750ec32e56746baa0c872ffeb
Maisie Ng <maisieng@Mac.home>
2025-03-03 16:30:04 -0800

    Add NewsEndpoint

---
+++ Headlinr/Networking/NewsEndpoint.swift
@@ -0,0 +1,61 @@
+//
+//  NewsEndpoint.swift
+//  Headlinr
+//
+//  Created by Maisie Ng on 3/3/25.
+//
+
+
+import Foundation
+
+enum NewsEndpoint{
+    case search(value: String)
+}
+
+
+extension NewsEndpoint: EndpointProtocol {
+    var baseURL: String {
+        return "https://newsapi.org"
+    }
+    
+    var path: String {
+        return "/v2/top-headlines"
+    }
+    
+    var parameter: [URLQueryItem] {
+        var queryItems: [URLQueryItem] = []
+        
+        switch self {
+        case .search(let country):
+            queryItems.append(URLQueryItem(name: "country", value: country))
+            queryItems.append(URLQueryItem(name: "apiKey",value: ""))
+        }
+        return queryItems
+    }
+    
+    var request: URLRequest? {
+        var urlComponent = URLComponents(string: baseURL)
+        urlComponent?.path = path
+        urlComponent?.queryItems = parameter
+        
+        if let urlString = urlComponent?.url {
+            var request = URLRequest(url: urlString)
+            request.httpMethod = httpMethod.rawValue
+            request.allHTTPHeaderFields = header
+            return request
+            
+        }
+        
+        return nil
+     
+    }
+    
+    var httpMethod: HTTPMethod {
+        return .get
+    }
+    
+    var header: [String : String] {
+        return ["Content-Type":"application/json"]
+    }
+
+}
